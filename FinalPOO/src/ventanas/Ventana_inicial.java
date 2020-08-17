23b3c99057ea3a9aeb8f5ba99652e991273f9104
RubenFuquene <rubendariofuquene@gmail.com>
2020-08-16 19:57:28 -0500

    Ventana inicial (funciona como prueba)

---
+++ FinalPOO/src/ventanas/Ventana_inicial.java
@@ -0,0 +1,43 @@
+package ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Ventana_inicial {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana_inicial window = new Ventana_inicial();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ventana_inicial() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
