0dd9e590b7b80ef0aa5914ba6aa286eee7ad7ba1
lenovo <lenovo@192.168.1.56>
2020-08-20 19:42:25 -0500

    Creación de clases de base de datos

---
+++ src/datos/GestorAsignaturas.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package datos;
+
+import java.util.ArrayList;
+
+import mundo.Asignatura;
+
+/**
+ * @author Andrés
+ *
+ */
+public class GestorAsignaturas {
+	
+	//objeto para leer archivos
+	
+	public GestorAsignaturas() {
+		
+		//Creación o apertura del archivo
+	}
+	
+	public boolean agregar_asignatura(Asignatura nuevaAsignatura) {
+		
+		return true;
+	}
+	
+	public boolean modificar_asignatura(Asignatura asignatura) {
+		
+		return true;
+	}
+	
+	public ArrayList<Asignatura> ver_estudiantes(){
+		
+		return new ArrayList<Asignatura>();
+	}
+}
---
+++ src/datos/GestorEncuestas.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package datos;
+
+import java.util.ArrayList;
+
+import mundo.Encuesta;
+
+/**
+ * @author Andrés
+ *
+ */
+public class GestorEncuestas {
+
+	//objeto para leer archivos
+	
+	public GestorEncuestas() {
+		
+		//Creación o apertura del archivo
+	}
+	
+	public ArrayList<Encuesta> almacenar_encuestas(){
+		
+		return new ArrayList<Encuesta>();
+	}
+	
+	public ArrayList<Encuesta> ver_encuestas(){
+		
+		return new ArrayList<Encuesta>();
+	}
+}
